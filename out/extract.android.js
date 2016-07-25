a1e713773aee1292801e559f357cdf9777e3cfbc
Brian Nicholson <bnicholson@mozilla.com>
2016-07-25 06:50:00 -1000

    Rename bin -> out

--- out/extract.android.js
+++ out/extract.android.js
@@ -6159,6 +6159,9 @@ return /******/ (function(modules) { // webpackBootstrap
 },{}],5:[function(require,module,exports){
 const { getMetadata } = require('page-metadata-parser');
 
+// TODO: Fails on iOS (regeneratorRuntime not defined).
+// We can fix this by requiring babel-polyfill, but we should figure out how to
+// make that an iOS-only require since Android is already ES6-compatible.
 const pageMetadata = getMetadata(document);
 const pageTitle = pageMetadata.title;
 
